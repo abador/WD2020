a8b0cf38137fe91b6e7b3ec07e9a945056b115a5
Bartosz <bartuzi97@gmail.com>
2020-03-06 19:04:24 +0100

    initail commit

---
+++ tees.py
@@ -0,0 +1,25 @@
+print("Hello World ")
+a = "Xd"
+print(a)
+a = 6
+print(a)
+a = .2
+print(a)
+a = True
+print(a)
+a = 6
+print(a)
+a = None
+print(a)
+# calc
+a = 5
+b = 2
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 // 2)
+print(5 % 2)
+print(5 ** 2)
+
+
