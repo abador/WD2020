4c124de38ed60b18f401b7456909a046f5bd5c81
sylwia12345678 <sylwia.anna.u@gmail.com>
2020-03-06 19:02:54 +0100

    ee

--- ghfg.py
+++ ghfg.py
@@ -1,1 +1,16 @@
-print("Hello")
+a = u"Helloł "
+print(a)
+a, b = 333, 56644.55
+print(a)
+a = 333 * 1.0 #fdfgdfg
+print(a, b)
+a = a/2
+print(a)
+a /= 2
+print(a)
+a = u"Helloł %(a)d" %{'a': b}
+print(a)
+lista = ['fdfs', "bvb", 233, 33.2]
+print(lista)
+slownik = {'a': b}
+print(slownik)
