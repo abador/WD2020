faa422be598711425da2fbc30810b4c410c76d4b
local <local@WMII-BNAI8Q4IAM>
2020-03-06 19:11:16 +0100

    jakieś zmiany

--- start.py
+++ start.py
@@ -1,1 +1,16 @@
-print("Hello")
+a = u"Helloł "
+print(a)
+a, b = 333, 562353.55
+print(a)
+a = 333 * 1.0 #aghgfasdhf
+print(a, b)
+a = a/2
+print(a)
+a /= 2
+print(a)
+a = u"Helloł %(a)d" %{'a': b}
+print(a)
+lista = ['sdsd', "sd", 233, 33.2]
+print(lista)
+slownik = {'a': b}
+print(slownik)
