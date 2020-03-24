a424166d2c6f7b164923dd6d4bbb0a4fb32f89db
Camer <camer@max-play.pl>
2020-03-24 14:09:13 +0100

    new version of cw3/zad11

--- cw3/zad11.py
+++ cw3/zad11.py
@@ -1,8 +1,7 @@
 # jakiś problem ze ścieżkami przez co problem z implementacją from zespolone import *
-# from WD2020.cw3.zespolone import dzialania, wyswietlanie
-from zespolone import *
+from WD2020.cw3.zespolone import dzialania, wyswietlanie
 z1=2+3j
 z2=8+10j
-print(zespolone.rzeczywista(z1))
-print(zespolone.urojona(z2))
-print(zespolone.dodaj(z1, z2))
+print(wyswietlanie.rzeczywista(z1))
+print(wyswietlanie.urojona(z2))
+print(dzialania.dodaj(z1, z2))
