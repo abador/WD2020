b289ed1c8f8d9438ca2f32512a56d8c5f37261d4
kacperski36 <kacperski36@wp.pl>
2020-04-20 17:41:36 +0200

    zadanie 6 zrobione

--- cw_4.py
+++ cw_4.py
@@ -39,3 +39,64 @@ gra=robaczek()
 gra.idz_w_gore(10)
 gra.idz_w_lewo(15)
 gra.pokaz_gdzie_jestes()
+
+# zad 6
+def czynalezy(a="",b=""):
+    for i in range(0,len(a)):
+        if(a[i]==b):
+            return True
+
+def unikalnosc(a=""):
+    wynik=""
+    for i in range(0,len(a)):
+        if(czynalezy(wynik,a[i])==None):
+            wynik+=a[i]
+    return wynik
+
+class slowa:
+    def __init__(self, slowo1,slowo2):
+        self.slowo1 = slowo1
+        self.slowo2=slowo2
+
+    def palindrom(self):
+        y = len(self.slowo1) - 1
+        for i in range(0, len(self.slowo1), 1):
+            if (self.slowo1[i] != self.slowo1[y]):
+                return print("To nie jest Palindrom")
+            else:
+                y -= 1
+        print("To jest Palindrom")
+
+    def metagram(self):
+        x=0
+        if(len(self.slowo1)==len(self.slowo2)):
+            for i in range(0, len(self.slowo2),1):
+                if(self.slowo1[i]!=self.slowo2[i]):
+                    x+=1
+            if x == 1:
+                print("To są metagramy")
+            else:
+                print("To nie sa metagramy")
+        else:
+            print("To nie sa metagramy")
+
+    def anagramy(self):
+        a = unikalnosc(self.slowo1)
+        b = unikalnosc(self.slowo2)
+        x = 0
+        for i in range(0, len(a)):
+            for j in range(0, len(b)):
+                if (a[i] == b[j]):
+                    x += 1
+        if x == len(a):
+            print("To jest anaram")
+        else:
+            print("To nie jest anagram")
+
+
+xd = "mata"
+xdd="tama"
+a = slowa(xd,xdd)
+a.palindrom()
+a.metagram()
+a.anagramy()
