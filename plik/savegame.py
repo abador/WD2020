9a88de145e0c5b96337397e99b2e057dedeabbfd
5362396 <5362396@wp.pl>
2020-04-23 12:59:30 +0200

    savegame.py

--- plik/savegame.py
+++ plik/savegame.py
@@ -10,13 +10,17 @@ def save(numer, pozycja):
 
 
 def load(numer):
-    if numer == 1:
-        plik = open("save1.py", "r")
-    elif numer == 2:
-        plik = open("save2.py", "r")
-    elif numer == 3:
-        plik = open("save3.py", "r")
-    odczytstr = plik.readline()
-    plik.close()
-    odczyt = eval(odczytstr)
-    return odczyt
+    try:
+        if numer == 1:
+            plik = open("save1.py", "r")
+        elif numer == 2:
+            plik = open("save2.py", "r")
+        elif numer == 3:
+            plik = open("save3.py", "r")
+        odczytstr = plik.readline()
+        plik.close()
+        odczyt = eval(odczytstr)
+        return odczyt
+    except FileNotFoundError:
+        print("Nie zapisales gry na tym pliku, rozpoczynanie nowej gry")
+        return 0
