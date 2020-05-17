38f455b79603e02e360ab584cf5733af5a5e25ea
mixvert <58787186+mixvert@users.noreply.github.com>
2020-05-17 18:40:07 +0200

    wd_cw06

--- cw_6.py
+++ cw_6.py
@@ -39,35 +39,86 @@ import numpy as np
 # print(foo5(7))
 
 # # ZADANIE 6
-# tab = np.array([np.array(['S', 'A', 'G']), np.array(['I', 'A', 'I']), np.array(['L', 'O', 'L'])])
-# print(tab)
+# tab1 = list(input())
+# tab2 = list(input())
+# tab3 = list(input())
+# tab_len = [len(tab1), len(tab2), len(tab3)]
+# tab_len.sort()
+# tab_len.reverse()
+# print(tab_len[0])
+# m = np.array([np.arange(tab_len[0]+1) for x in range(tab_len[0]+1)])
+# m = m.astype('unicode_')
+# for x in range(1,tab_len[0] + 1):
+#     if x <= len(tab1):
+#         m[0][x] = tab1[x-1]
+#     if x <= len(tab2):
+#         m[x][0] = tab2[x-1]
+#     if x <= len(tab3):
+#         m[x][x] = tab3[x-1]
+# print(m)
 
-# ZADANIE 7
-# Napisz funkcję, która wygeneruje macierz wielowymiarową postaci:
-# [[2 4 6]
-#  [4 2 4]
-#  [6 4 2]]
+
+# # ZADANIE 7
+# def foo7(n):
+#     tab_temp = []
+#     tab = np.zeros((n, n))
+#     for z in range(n):
+#         y = np.array([np.diag([2 * (z + 1) for x in range(n-z)], z)])
+#         tab_temp.append(y)
+#         if z != 0:
+#             y = np.array([np.diag([2 * (z +1) for x in range(n-z)], -z)])
+#             tab_temp.append(y)
+#     tab_temp = np.asarray(tab_temp)
+#     for x in range((2 * n) -1):
+#         for y in range(n):
+#             for z in range(n):
+#                 if tab_temp[x][0][y][z] > 0:
+#                     tab[y][z] = tab_temp[x][0][y][z]
 #
-# Przy założeniach:
-# - funkcja przyjmuje parametr n, który określa wymiary macierzy jako n*n i umieszcza
-# wielokrotność liczby 2 na kolejnych jej przekątnych rozchodzących się od głównej przekątnej.
+#     return tab.astype('int64')
+#
+# print(foo7(3))
 
-def foo7(n):
-    tab_temp = []
-    tab = np.zeros((n, n))
-    for z in range(n):
-        y = np.array([np.diag([2 * (z + 1) for x in range(n-z)], z)])
-        tab_temp.append(y)
-        if z != 0:
-            y = np.array([np.diag([2 * (z +1) for x in range(n-z)], -z)])
-            tab_temp.append(y)
-    tab_temp = np.asarray(tab_temp)
-    for x in range((2 * n) -1):
-        for y in range(n):
-            for z in range(n):
-                if tab_temp[x][0][y][z] > 0:
-                    tab[y][z] = tab_temp[x][0][y][z]
+# # ZADANIE 8
+# def wypelnianie_tablicy(x,y):
+#     tablica = np.empty((x,y))
+#     for x1 in range(x):
+#         for y1 in range(y):
+#             tablica[x1, y1] = int(input("Podaj liczbę: "))
+#     return tablica
+#
+# def foo8(tablica, kierunek):
+#     if kierunek.lower() == 'poziomo':
+#         if tablica.shape[0] % 2 == 0:
+#             tablica = tablica[:int(tablica.shape[0] / 2)]
+#             return tablica.astype('int64')
+#     elif kierunek.lower() == 'pionowo':
+#         if tablica.shape[1] % 2 == 0:
+#             tablica = tablica[:,:int(tablica.shape[1] / 2)]
+#             return tablica.astype('int64')
+#
+# while True:
+#      try:
+#          x = input("Podaj ilość rzędów: ")
+#          y = input("Podaj ilość kolumn: ")
+#          z = wypelnianie_tablicy(int(x), int(y))
+#          kierunek = input("Podaj kierunek [POZIOMO / PIONOWO]: ")
+#          if kierunek.lower() == 'poziomo' or kierunek.lower() == 'pionowo':
+#             break
+#      except ValueError:
+#          print("Oops!  That was no valid number.  Try again...")
+# print(foo8(z, kierunek))
 
-    return tab.astype('int64')
+# ZADANIE 9
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(foo7(3))
+lista = []
+for x in range(25):
+    lista.append(fibonacci(x + 1))
+lista = np.array(lista)
+lista = lista.reshape((5,5))
+print(lista)
